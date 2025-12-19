fa14496c12fba88d16c6e584aff0102904a75e8e
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2025-12-19 10:06:42 +0530

    Revert "chore: add api prefix"
    This reverts commit 698a18c639a1c5ea4c67d7526a5369611f3f1d43.

--- app/main.py
+++ app/main.py
@@ -107,7 +107,7 @@ app.add_middleware(
 
 
 # Include routers
-app.include_router(attendance_router, prefix="/api/v1/attendance")
+app.include_router(attendance_router, prefix="/api/v1")
 
 
 # Health check endpoint
